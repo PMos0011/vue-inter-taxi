ce5a24d5c57281b0b8a749718dccdf3c4810c2fa
Paweł <pawel.moskwa.pm@gmail.com>
2019-12-03 11:00:56 +0100

    Add mobile navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app-bar prominent fixed shrink-on-scroll elevate-on-scroll color="#ffd54c">
-    <template v-slot:img >
+    <template v-slot:img>
       <v-img
         src="../images/inter_logo.webp"
         height="60%"
@@ -8,19 +8,70 @@
         position="left"
         class="ma-4"
       ></v-img>
-      
     </template>
-    <template v-slot:extension 
-      ><v-tabs class="mb-12" background-color="transparent">
+    <template v-slot:extension
+      ><v-toolbar color="transparent" class="mb-12" flat>
         <v-spacer></v-spacer>
-        <v-btn large text rounded class="mr-2 headline font-weight-bold" >usługi</v-btn>
-        <v-btn large text rounded class="mr-2 headline font-weight-bold">o nas</v-btn>
-        <v-btn large class="mr-4  headline font-weight-bold" rounded dark href="tel: +48757719919"
-          ><v-icon large class="ml-0 mr-3" right dark>mdi-phone</v-icon>zadzwoń 757
-          719 919 lub 505 159 191</v-btn
-        >
-      </v-tabs></template
-    >
+
+        <v-toolbar class="mb-12 hidden-md-and-down" color="transparent" flat>
+          <v-spacer></v-spacer
+          ><v-btn large text rounded class="mr-2 headline font-weight-bold"
+            >o nas</v-btn
+          >
+          <v-btn large text rounded class="mr-2 headline font-weight-bold"
+            >usługi</v-btn
+          >
+          <v-btn
+            large
+            class="mr-4  headline font-weight-bold"
+            rounded
+            dark
+            href="tel: +48757719919"
+            ><v-icon large class="ml-0 mr-3" right dark>mdi-phone</v-icon
+            >zadzwoń 757 719 919 lub 505 159 191</v-btn
+          >
+        </v-toolbar>
+
+        <v-menu>
+          <template v-slot:activator="{ on }">
+            <v-app-bar-nav-icon
+              large
+              class="hidden-lg-and-up"
+              v-on="on"
+            ></v-app-bar-nav-icon>
+          </template>
+          <v-list color="#ffd54c"
+            ><v-list-item>
+              <v-list-item-title
+                ><v-btn text rounded class="mr-2 font-weight-bold"
+                  >o nas</v-btn
+                ></v-list-item-title
+              ></v-list-item
+            >
+            <v-list-item>
+              <v-list-item-title
+                ><v-btn text rounded class="mr-2 font-weight-bold"
+                  >usługi</v-btn
+                ></v-list-item-title
+              ></v-list-item
+            >
+            <v-list-item>
+              <v-list-item-title>
+                <v-btn
+                  large
+                  class="mr-4 font-weight-bold"
+                  rounded
+                  dark
+                  href="tel: +48757719919"
+                  ><v-icon class="ml-0 mr-3" right dark>mdi-phone</v-icon
+                  >zadzwoń</v-btn
+                ></v-list-item-title
+              ></v-list-item
+            ></v-list
+          >
+        </v-menu>
+      </v-toolbar>
+    </template>
   </v-app-bar>
 </template>
 <script></script>
